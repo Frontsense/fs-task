ae644c5010819ef1b49874358d90b7624d94bd17
asrsa <aljaz.srsa@gmail.com>
2020-06-26 15:17:08 +0200

    added service discovery etcd

--- api/src/main/java/com/mag/frontsense/resources/TaskResource.java
+++ api/src/main/java/com/mag/frontsense/resources/TaskResource.java
@@ -1,5 +1,6 @@
 package com.mag.frontsense.resources;
 
+import com.kumuluz.ee.discovery.annotations.DiscoverService;
 import com.mag.frontsense.beans.TaskBean;
 import com.mag.frontsense.models.Task;
 import org.json.JSONObject;
@@ -7,6 +8,7 @@ import org.json.JSONObject;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -18,7 +20,33 @@ import java.util.List;
 public class TaskResource {
 
     @Inject
+    @DiscoverService(value = "fs-user", version = "1.0.x", environment = "dev")
+    private WebTarget userTarget;
+
+    @Inject
     private TaskBean taskBean;
+
+    @GET
+    @Path("url")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getUrl() {
+        return Response.ok(userTarget.getUri().toString()).build();
+    }
+
+    @GET
+    @Path("users")
+    public Response getUsers() {
+        WebTarget userService = userTarget.path("user/all");
+
+        Response response;
+        try {
+            response = userService.request().get();
+        } catch (ProcessingException e) {
+            return Response.status(408).build();
+        }
+
+        return Response.ok(response.readEntity(String.class)).build();
+    }
 
     @GET
     @Path("/test")
